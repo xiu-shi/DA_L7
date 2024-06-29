ca4d83059e56a1f1dd826723de36e512c46696b0
xiu-shi <eupirate@gmail.com>
2024-06-28 21:36:36 -0400

    ML_ca2

--- L7_term3/L7_ML_CA2/ml_ca2_3.py
+++ L7_term3/L7_ML_CA2/ml_ca2_3.py
@@ -1,8 +1,7 @@
 
 
-### 3. Classification and Anomaly Detection
+### 3. Classification Model 
 
-import pandas as pd
 
 ### load data
 csv = '/Users/janet.xuishi/Documents/DA_L7/DA_L7/L7_term3/L7_ML_CA2/IT_Company_System_Issues_Classification.csv'
@@ -41,11 +40,70 @@ label_encoder = LabelEncoder()
 encoded_target = label_encoder.fit_transform(target)
 
 ### Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(all_features, encoded_target, test_size=0.2, random_state=42)
+
+
+###((800, 22), (200, 22), (800,), (200,))
+
+### Plot the distribution of:
+### Issue_Type, System_Component, and Customer_Impact 
+### with respect to Priority
+
+plt.figure(figsize=(12, 6))
+sns.countplot(x='Issue_Type', hue='Priority', data=data)
+plt.title('Distribution of Issue_Type by Priority')
+plt.xlabel('Issue Type')
+plt.ylabel('Count')
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(12, 6))
+sns.countplot(x='System_Component', hue='Priority', data=data)
+plt.title('Distribution of System_Component by Priority')
+plt.xlabel('System Component')
+plt.ylabel('Count')
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(12, 6))
+sns.countplot(x='Customer_Impact', hue='Priority', data=data)
+plt.title('Distribution of Customer_Impact by Priority')
+plt.xlabel('Customer Impact')
+plt.ylabel('Count')
+plt.tight_layout()
+plt.show()
+
+
+
+### Our dataset has been successfully preprocessed and split into training and test sets in 80/20 ratio
+### Training set: 800 samples, 22 features
+### Testing set: 200 samples, 22 features
+
+### Build model: our existing features can be encoded, we’ll train a random forest classifier with the preprocessed data.
+
+from sklearn.ensemble import RandomForestClassifier
+
+### Initialize the Random Forest Classifier
+classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+
+### Train the model
+classifier.fit(X_train, y_train)
+
+### Evaluate our model
+
+from sklearn.metrics import classification_report
+
+### Predict on the test set
+y_pred = classifier.predict(X_test)
+
+### Evaluate the model
+report = classification_report(y_test, y_pred, target_names=label_encoder.classes_)
+print(report)
 
 
 
 
+
+### 4. Anomaly(outlier) Detection Algorithm
 
 
 
